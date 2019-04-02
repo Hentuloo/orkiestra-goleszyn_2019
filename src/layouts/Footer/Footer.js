9b50d4407b2c758cef783aaa43162a14539fe3f6
Kamil <Kamil@>
2019-04-02 18:55:07 +0200

    footer template and for mobile

--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -3,8 +3,22 @@ import React from 'react';
 
 const Footer = () => {
     return ( 
-        <footer>
-            Tu jest stopka mordo
+        <footer className="footer">
+            <section>
+                <h1>Orkiestra Goleszyn</h1>
+                <h2>orkiestra.goleszyn@gmail.com</h2>
+                <p>Białyszewo ul.Niepodległości 4</p>
+                <a href="#FB"><div className="fb-link">F</div></a>
+            </section>
+            <section>
+                <h1>Powiązane strony:</h1>
+                <a href="#jakiś link">Młodzieżowa orkiestra G</a>
+            </section>
+            <section>
+                <h1>Organizacje:</h1>
+                <img src="" alt=""/>
+            </section>
+            <div>Wszystkie prawa zastrzeżone. Copyright by OSP Goleszyn</div>
         </footer>
      );
 }
