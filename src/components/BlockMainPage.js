e1623e33cff4b30f8cac92f2dffeddce6bc4c73a
Kamil <Kamil@>
2019-04-12 20:17:37 +0200

    Correct sfc where i use unnecessary return

--- src/components/BlockMainPage.js
+++ src/components/BlockMainPage.js
@@ -1,15 +1,13 @@
 import React from "react"
 
-const BlockMainPage = ({ data: props }) => {
-	return (
-		<article className="block-article">
-			<div>{props.content}</div>
-			<div>
-				<img src={props.picture.url} alt={props.pictureTitle} />
-				<h1>{props.pictureTitle}</h1>
-			</div>
-		</article>
-	)
-}
+const BlockMainPage = ({ data: props }) => (
+	<article className="block-article">
+		<div>{props.content}</div>
+		<div>
+			<img src={props.picture.url} alt={props.pictureTitle} />
+			<h1>{props.pictureTitle}</h1>
+		</div>
+	</article>
+)
 
 export default BlockMainPage
--- src/components/ContactBlock.js
+++ src/components/ContactBlock.js
@@ -1,15 +1,13 @@
 import React from "react"
 
-const ContactBlock = ({ data }) => {
-	return (
-		<section>
-			<div className="contact-image">
-				<img src={data.bgcIcon.url} alt="" />
-			</div>
-			<h1>{data.title}</h1>
-			<div>{data.content}</div>
-		</section>
-	)
-}
+const ContactBlock = ({ data }) => (
+	<section>
+		<div className="contact-image">
+			<img src={data.bgcIcon.url} alt="" />
+		</div>
+		<h1>{data.title}</h1>
+		<div>{data.content}</div>
+	</section>
+)
 
 export default ContactBlock
