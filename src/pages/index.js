115be1737abb5b0a73d491b9820c38ef1437d585
Kamil <Kamil@>
2019-07-23 17:03:09 +0200

    remove useless import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
-import "../styles/index.sass"
 
 //layout: header with navigation and foother
 import Header from "../layouts/Header/Header"
